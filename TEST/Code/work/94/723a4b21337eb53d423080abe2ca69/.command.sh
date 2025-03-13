40787f2fdf938b7152bba8eeefd04bc63b2dfb97
Kohaku0102 <dangtranduymdc@gmail.com>
2025-03-13 12:25:16 +0700

    delete log

--- TEST/Code/work/94/723a4b21337eb53d423080abe2ca69/.command.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ue
-fastp \
--i DRR187559_1.fastq.gz -I DRR187559_2.fastq.gz -o DRR187559.cleaned.R1.fastq.gz -O DRR187559.cleaned.R2.fastq.gz \
---json DRR187559.fastp.json \
---html DRR187559.fastp.html \
- --thread 3 --detect_adapter_for_pe \
- --qualified_quality_phred 20 \
- --cut_right  --length_required 50
--- TEST/Code/work/cc/e008a4e9e5ce27a8de79083ea6978f/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-spades.py -1 DRR187559.cleaned.R1.fastq.gz -2 DRR187559.cleaned.R2.fastq.gz -o Result_DRR187559 --only-assembler
